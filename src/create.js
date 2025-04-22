06b6344dbee15a1bfe50524c3d9db420ade35ecd
Anil Sharma <anil.sharma@webanix.in>
2025-04-22 17:02:02 +0530

    structure

---
+++ src/create.js
@@ -0,0 +1,20 @@
+// src/create.js
+
+// async function create(model, data) {
+//     try {
+//       const result = await model.create(data);
+//       console.log("✅ Created:", result.toJSON());
+//       return result;
+//     } catch (error) {
+//       console.error("❌ Error:", error.message);
+//       throw error;
+//     }
+//   }
+
+function create(payload) {
+    console.log("Created:", payload);
+    return { status: "success", data: payload };
+  }
+  
+  module.exports = create;
+  
---
+++ src/index.js
@@ -0,0 +1,9 @@
+// src/index.js
+
+const create = require('./create');
+// const update = require('./update'); // if you add more
+
+module.exports = {
+  create,
+  // update
+};
